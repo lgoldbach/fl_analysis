f128dd74824c984ac0417281600f66bc17ea5e1c
lgoldbach <leander.goldbach@gmail.com>
2023-09-29 11:18:29 +0200

    basic snake pipeline

---
+++ fl_analysis/utils.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+
+
+def read_fl_data(path):
+    """Read the data from the fitness landscape data file
+
+    Args:
+        path (str): Path to the fitness landscape data file
+    
+    Returns:
+        (np.array, np.array): array of genotypes, array of phenotypes 
+    """
+    fl_df = pd.read_csv(path, delimiter="\t")
+
+    # turn into N x L array (N = #sequences, L = sequence length)
+    seq_arr = np.array([list(seq) for seq in fl_df["seq"]])
+    # remove columns that contain the same base, i.e. redundant sites of seq.
+    seq_arr = remove_redundant(seq_arr)
+
+    phe_arr = fl_df["score"].to_numpy(dtype=float)
+
+    return seq_arr, phe_arr
+
+
+def remove_redundant(arr):
+    """Remove redundant columns from sequence array, i.e. an array with 
+    dimensions NxL, where N is the number of sequences and L the seq. length.
+
+    Args:
+        arr (np.array): NxL array, N: number of sequences, L:
+                                   sequence length.
+    Returns:
+        arr: np.ndarray
+            output array without redundant columns
+    """
+    nonredundant_cols = []
+    for i in range(arr.shape[1]-1):
+        unique_elements = np.unique(arr[:, i])
+        if len(unique_elements) > 1:
+            nonredundant_cols.append(i)
+    arr = arr[:, nonredundant_cols]
+    return arr
+
+
+def sequence_to_integers(sequence):
+    """Turn an ATGC sequence into integer sequence.
+    e.g. [[T, T, A, G, C],  -->  [[2, 2, 1, 3, 4],
+          [A, G, C, T, T]]        [1, 3, 4, 2, 2]]
+
+    Args:
+        sequence (str): ATGC based sequence
+
+    Returns:
+        np.array: Array where bases (str) are replaced by integers
+
+    """
+    sequence_ = np.copy(sequence)
+    sequence_[sequence_ == 'A'] = 1
+    sequence_[sequence_ == 'T'] = 2
+    sequence_[sequence_ == 'G'] = 3
+    sequence_[sequence_ == 'C'] = 4
+
+    return sequence_
+
+
+def kimura_fixation(p1: float, p2: float, N: int):
+    """Formula to compute kimuara's fixation probability
+
+    Args:
+        p1 (float): Phenotype 1
+        p2 (float): Phenotype 2
+        N (int): Population size
+
+    Returns:
+        float: fixation probability (float in [0, 1]).
+    
+    """
+    s = p2 - p1
+    return (1-np.exp(-2*s))/(1-np.exp(-2*N*s))
+
---
+++ scripts/csv_to_array.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import numpy as np
+
+from fl_analysis.utils import read_fl_data, remove_redundant, sequence_to_integers
+
+# Snakemake input output definitions
+input = snakemake.input[0]
+output_genotypes = snakemake.output.genotypes
+output_genotypes_num = snakemake.output.genotypes_num
+output_phenotypes = snakemake.output.phenotypes
+
+seq_arr, phe_arr = read_fl_data(input)
+seq_arr_num = sequence_to_integers(seq_arr)  # encode bases as integers
+
+np.save(output_genotypes, seq_arr)
+np.save(output_genotypes_num, seq_arr_num)
+np.save(output_phenotypes, phe_arr)
